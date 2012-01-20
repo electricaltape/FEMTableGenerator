72e143a456646534112280e7d74d972f62966864
dave <drwells@vt.edu>
2012-01-19 22:42:17 -0500

    cleaned up output of tables from BASH

--- generateTables.sh
+++ generateTables.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/env bash
 # polynomials
-SAGE='/Users/drwells/Applications/sage/sage'
+#SAGE='/Users/drwells/Applications/sage/sage'
+SAGE='sage'
+
 for i in {1..10}
 do
     $SAGE printTables.sage --order=$i --derivative=0 | grep '0' | \
-    sed "s/\]\],/]],\\`echo -e '\n\r'`/g" > Tables/polynomial_$i.txt
+    sed 's/\],/],\n/g' > Tables/polynomial_$i.txt
 done
 
 # gradients
 for i in {1..10}
 do
     $SAGE printTables.sage --order=$i --derivative=1 | grep '0' | \
-    sed "s/\]\],/]],\\`echo -e '\n\r'`/g" > Tables/gradient_$i.txt
+    sed 's/\],/],\n/g' > Tables/gradient_$i.txt
 done
